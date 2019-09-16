f10df6ba575409945146e3ab0589761b8b879ae1
Francois 27139433 <27139433@student.g.nwu.ac.za>
2019-09-16 19:52:24 +0200

    Add Components to Home pages
    Login and Register component

---
+++ DatingApp/DatingApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.authService.register(this.model).subscribe(() => {
+      console.log('Registration Succesfull');
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  cancel() {
+    console.log('concelled');
+  }
+
+}
